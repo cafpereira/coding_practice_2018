d443b0db3122b2c0a7599ef1d5357896962207a4
Carlos Pereira <carlos_pereira@ultimatesoftware.com>
2018-03-11 21:48:10 -0400

    count inversions NlogN

--- interview-bit/17_prob_jump_lvl7/count_inversions.py
+++ interview-bit/17_prob_jump_lvl7/count_inversions.py
@@ -2,3 +2,50 @@ class Solution:
     # @param A : list of integers
     # @return an integer
     def countInversions(self, A):
+        n = len(A)
+        return self.countInversionsHelper(A, 0, n - 1)
+
+    def countInversionsHelper(self, A, start, end):
+        if start >= end:
+            return 0
+
+        mid = (start + end) // 2
+
+        left = self.countInversionsHelper(A, start, mid)
+        right = self.countInversionsHelper(A, mid + 1, end)
+        split = self.merge(A, start, mid, end)
+
+        return left + right + split
+
+    def merge(self, A, start, mid, end):
+        M = []
+        L = A[start: mid + 1]
+        H = A[mid + 1: end + 1]
+
+        split = 0
+        while L and H:
+            if L[0] <= H[0]:
+                M.append(L.pop(0))
+            else:
+                split += len(L)
+                M.append(H.pop(0))
+
+        if L:
+            for v in L:
+                M.append(v)
+        else:
+            for v in H:
+                M.append(v)
+
+        for i in range(start, end + 1):
+            A[i] = M.pop(0)
+
+        return split
+
+
+s = Solution()
+# A = [1 , 2, 1]
+A = [2, 4, 1, 3, 5]
+
+print(s.countInversions(A))
+print(A)
